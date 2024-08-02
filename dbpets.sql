6b1152366b32446635a98c43b43068cb7bded50a
italo <italorosa.profissional@gmail.com>
2024-08-02 19:28:55 -0300

    first commit

---
+++ dbpets.sql
@@ -0,0 +1,8 @@
+--apagar banco antes de recriar ele pelo script 
+drop database dbPets;
+-- criar banco
+create database dbPets;
+-- acessar banco criado
+use dbPets;
+-- visualizar banco 
+show databases;
