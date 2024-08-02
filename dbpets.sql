a56c8d35046f05de2ee290f93bf04ffc4d20857f
italo <italorosa.profissional@gmail.com>
2024-08-02 20:13:08 -0300

    script

--- dbpets.sql
+++ dbpets.sql
@@ -7,13 +7,27 @@ use dbPets;
 -- visualizar banco 
 show databases;
 -- criando tabelas. 
+create table tbFuncionarios(
+codFunc int not null auto_increment,
+nome varchar(100) not null,
+email varchar(100),
+telCel char(10),
+cpf char(14),
+primary key(codFunc)
+);
+
 create table tbUsuarios(
 codUsu int not null auto_increment,
 nome varchar(50) not null,
 senha varchar(10) not null,
-primary key(codUsu)
+codFunc int not null,
+primary key(codUsu),
+foreign key(codFunc)references tbFuncionarios(codFunc)
 );
+
+
 -- visualizando tabelas
 show tables;
 -- descricao das tabelas 
 desc tbUsuarios;
+desc tbFuncionarios;
